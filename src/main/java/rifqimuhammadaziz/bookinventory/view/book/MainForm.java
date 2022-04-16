6bc3eaf731b6d1991563687701625104e7c23084
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-04-16 14:40:44 +0700

    VIEW : Refactoring some ui component

--- src/main/java/rifqimuhammadaziz/bookinventory/view/book/MainForm.java
+++ src/main/java/rifqimuhammadaziz/bookinventory/view/book/MainForm.java
@@ -1,12 +1,41 @@
 package rifqimuhammadaziz.bookinventory.view.book;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 import javax.swing.*;
 
 public class MainForm {
     private JButton loginButton;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
+    private JTextField txtID;
+    private JTextField txtBookCode;
+    private JTextField txtTitle;
+    private JTextField txtWriter;
+    private JTextField txtPublisher;
+    private JPanel rootPanel;
+    private JTextField txtISBN;
+    private JTable table1;
+    private JTextField txtBuyPrice;
+    private JTextField txtSellPrice;
+    private JTextField txtDateOfEntry;
+    private JTextField txtStock;
+    private JButton btnNew;
+    private JButton btnSave;
+    private JButton btnReset;
+    private JButton btnUpdate;
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new FlatLightLaf());
+        UIManager.put( "Button.arc", 999 );
+        UIManager.put( "Component.arc", 999 );
+        UIManager.put( "ProgressBar.arc", 999 );
+        UIManager.put( "TextComponent.arc", 999 );
+        UIManager.put( "Component.focusWidth", 1 );
+        UIManager.put( "Table.showHorizontalLines", true);
+
+        JFrame frame = new JFrame("MainForm");
+        frame.setContentPane(new MainForm().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
