a99298cf5ec7979443b5278a6f26021b64ab74c9
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-04-16 14:06:16 +0700

    PROJECT : Add Flatlaf JFormDesigner & Progress View MainForm

---
+++ src/main/java/rifqimuhammadaziz/bookinventory/view/book/MainForm.java
@@ -0,0 +1,12 @@
+package rifqimuhammadaziz.bookinventory.view.book;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JButton loginButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+}
